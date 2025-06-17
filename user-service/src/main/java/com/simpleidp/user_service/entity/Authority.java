b3da9c4121ee5de6fac98a495586991667c411a4
Spoorthi S <126961081+Spoooo8@users.noreply.github.com>
2025-06-17 13:50:53 +0530

    added user service

---
+++ user-service/src/main/java/com/simpleidp/user_service/entity/Authority.java
@@ -0,0 +1,28 @@
+package com.simpleidp.user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="authorities")
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name="customer_id", nullable = false)
+    private Customer customer;
+
+
+}
---
+++ user-service/src/main/java/com/simpleidp/user_service/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.simpleidp.user_service.repository;
+
+
+import com.simpleidp.user_service.entity.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    Optional<Customer> findByEmail(String email);
+
+}
